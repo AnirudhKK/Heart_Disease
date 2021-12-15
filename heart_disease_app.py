c71a148bf2127060598ff5cbe4ce8a23ab4bb7a3
Anirudh Kumar Kushwaha <49266081+AnirudhKK@users.noreply.github.com>
2021-12-15 10:22:24 +0530

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -39,6 +39,7 @@ def predict():
 
 if __name__ == '__main__':
 #Run the application
-    app.run()
+    port = os.environ.get("PORT", 5000)
+app.run(debug = False, host = "0.0.0.0", port=port)
     
     
